979a0a6e9acea574b71ae63ec8231f967e3723f0
SD6399 <david-shapiro@mail.ru>
2023-03-02 13:54:09 +0300

    add except

--- reedsolomon.py
+++ reedsolomon.py
@@ -1,5 +1,5 @@
 import numpy as np
-from reedsolo import RSCodec, rs_encode_msg, init_tables
+from reedsolo import RSCodec
 from PIL import Image
 from helper_methods import small2big, big2small
 from skimage import io
@@ -51,46 +51,17 @@ def extract_RS(image_RS, rsc):
         final_extract += ch
 
     # print(final_extract,len(final_extract))
-    rmes1, rmesecc1, errata_pos1 = rsc.decode(final_extract)
-    print(rmes1)
-    print(len(mes))
-    print(rsc.nsym / 2 / len(mes))
+    try:
+        rmes1, rmesecc1, errata_pos1 = rsc.decode(final_extract)
+        print(rmes1)
+        print(list(errata_pos1))
+    except:
+        print("Error of decoder")
 
 
 rsc = RSCodec(nsym=28, nsize=31)
 mes = 2 * b'Correct extraction of watermark from this video'
 # create_RS_cod(mes, rsc)
-extr_RS=io.imread(r"C:\Users\user\PycharmProjects\phase_wm\RS_cod89x89.png")
-extract_RS(extr_RS,rsc)
-
-# size = 8
-# bValues = [binaryString[i:i+size] for i in range(0, len(binaryString), size)]
-# string = ""
-# for bValue in bValues:
-#     integer = int(bValue, 2)
-#     character = chr(integer)
-#     print(character)
-#     string += character
-
-
-# import pyreedsolomon
-#
-# import numpy as np
-#
-# rs_dr = pyreedsolomon.Reed_Solomon(8,223,255,0x11D,0,1,32)
-#
-# data = np.random.randint(0,256,150).astype(np.uint8)
-#
-# data_enc = rs_dr.encode(data)
-#
-# # create a few errors
-# err_idx = [23,53,12,97,102]
-#
-#
-# data_enc[err_idx] = 255
-#
-# data_dec, n_errors = rs_dr.decode(data_enc)
-#
-# verify = np.all(data_dec==data)
-#
-# print(f"Decoding succes: {verify}. errors corrected {n_errors}")
+# extr_RS=io.imread(r"C:\Users\user\PycharmProjects\phase_wm\RS_cod89x89.png")
+# extr_RS[0:512,0:512]=0
+# extract_RS(extr_RS,rsc)
