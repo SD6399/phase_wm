62e2b8f1faaffa5c24efd438a518db946220fb9b
SD6399 <david-shapiro@mail.ru>
2023-03-02 12:15:52 +0300

    add normal func of embed and extract thaks RS

--- reedsolomon.py
+++ reedsolomon.py
@@ -1,41 +1,67 @@
-from reedsolo import RSCodec,rs_encode_msg,init_tables
+import numpy as np
+from reedsolo import RSCodec, rs_encode_msg, init_tables
+from PIL import Image
+from qrcode_1 import small2big, big2small
+from skimage import io
 
-# init_tables(0x11d)
-# nk = 2
-# packet= rs_encode_msg('123456789', nk)
+def create_RS_cod(mes, rsc):
+    tmp = rsc.encode(mes)
+
+    rmes, rmesecc, errata_pos = rsc.decode(tmp)
+
+    extract_txt_1 = []
+
+    for i in range(len(rmesecc)):
+        extract_txt_1.append((rmesecc[i]))
+
+    listbin = [bin(byte)[2:] for byte in extract_txt_1]
+    s = 0
+    binstr = np.array([])
+    for i in range(len(listbin)):
+        while len(listbin[i]) < 8:
+            listbin[i] = '0' + listbin[i]
+        s += len(listbin[i])
+        binstr = np.append(binstr, list(listbin[i]))
+
+    binstr = binstr.astype(int)
+    while len(binstr) < 89 * 89:
+        binstr = np.append(binstr, 0)
+
+    small_matrix = np.resize(binstr, (89, 89))
+    matr4embed = small2big(small_matrix)
+    matr4embed[matr4embed == 1] = 255
+    print(s)
+    img = Image.fromarray(matr4embed.astype('uint8'))
+    img.convert('RGB').save(r"C:\Users\user\PycharmProjects\phase_wm\RS_cod89x89.png")
 
 
-rsc = RSCodec(nsym=28,nsize=31)
+# The input should be a binary image
+def extract_RS(image_RS, rsc):
+    final_extract = b""
+    # print(len(extract_txt),len(rmesecc))
+    # final_extract_b=bytearray(final_extract,'utf-8')
+    matrbin = big2small(image_RS)
+    matrbin[matrbin == 255] = 1
+    listbin = np.reshape(matrbin, (89*89))
+    listbin = listbin.astype(int)
+    for i in range(0,len(listbin)-8,8):
+        tmp = ''.join(str(x) for x in listbin[i:i+8])
+        ch = bytes([(int(tmp, 2))])
+        final_extract += ch
 
+    # print(final_extract,len(final_extract))
+    rmes1, rmesecc1, errata_pos1 = rsc.decode(final_extract)
+    print(rmes1)
+    print(len(mes))
+    print(rsc.nsym / 2 / len(mes))
+
+
+rsc = RSCodec(nsym=28, nsize=31)
 mes = 2 * b'Correct extraction of watermark from this video'
-tmp = rsc.encode(mes)
-print(tmp)
-tmp[72] = 5
-tmp[4] = 5
-tmp[5] = 5
-tmp[7] = 5
-tmp[6] = 5
-
-tmp[-1] = 1
-rmes, rmesecc, errata_pos = rsc.decode(tmp)
-print(rsc.check(tmp))
-print(rsc.check(rmesecc))
-print(len(tmp), tmp)
-print(rmesecc)
-print(rmes)
-bytes1= rmesecc
-bin_str=(bin(int(rmesecc.hex(), 16)))
-print(len(bin_str))
-#
-listbin=[bin(byte) for byte in bytes1]
-binaryString = bin_str[2:]
-
-for el in listbin:
-    integer = int(el, 2)
-    character = chr(integer)
-
-print(len(mes))
-print(rsc.nsym/2/len(mes))
+# create_RS_cod(mes, rsc)
+extr_RS=io.imread(r"C:\Users\user\PycharmProjects\phase_wm\RS_cod89x89.png")
+extr_RS[7:10,111:120]=126
+extract_RS(extr_RS,rsc)
 
 # size = 8
 # bValues = [binaryString[i:i+size] for i in range(0, len(binaryString), size)]
