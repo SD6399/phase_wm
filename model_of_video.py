94995b6ba361d0e59e62caeb2b94e898282df27e
SD6399 <david-shapiro@mail.ru>
2023-06-30 17:07:04 +0300

    correctly calculation of ACF by DFT

--- model_of_video.py
+++ model_of_video.py
@@ -100,6 +100,20 @@ def ACF_image():
 
 
 # ACF(x, y) = ∑[I(i, j) * I(i + x, j + y)]
+def ACF_by_periodogram(lst):
+    lst_pix = np.array(lst)
+
+    # fft = np.fft.fft2(image_array)
+    fft = np.fft.fft2(lst_pix)
+    print(fft[0,0])
+    fft[0]=0
+    # fft[0][0]=0
+    abs_fft = np.power(np.abs(fft), 2)
+    ifft = np.fft.ifft2(abs_fft)
+    ifft = np.abs(ifft) / fft.size
+    print(ifft.shape,len(fft))
+
+    return ifft
 
 
 def calc_ACF(coord_x, coord_y, numb_frame):
@@ -122,40 +136,73 @@ if __name__ == '__main__':
     # part_1d = bracket_2()
     vidcap = cv2.VideoCapture("Road.mp4")
     success = True
-    list_00 = []
+    list_10 = []
+    list_500=[]
+    list_1000=[]
     count = 0
-    while success and count < 10:
-        success, image = vidcap.read()
-        if success:
-            list_00.append(image[10, 10, 0])
-        count += 1
-    image_array = io.imread("frames_orig_video/frame0.png")[:, :, 0]
+    # while success:
+    #     success, image = vidcap.read()
+    #     if success:
+    #         list_10.append(image[10, 10, 0])
+    #         list_500.append(image[500, 500, 0])
+    #         list_1000.append(image[1000, 1000, 0])
+    #         print(count)
+    #     count += 1
+    image_array = io.imread("frames_orig_video/frame2200.png")[:, :, 0]
     variance_list=np.var(image_array,axis=1)
     image_array_rav = np.ravel(image_array)
-    fft = np.fft.fft2(image_array)
-    fft[0][0]=0
-    abs_fft = np.power(np.abs(fft), 2)
-    ifft = np.fft.ifft2(abs_fft)
-    ifft = np.abs(ifft)/len(fft)
-    print(fft.shape, fft[0:50])
-    for i in range(0,1001,300):
-        plt.plot(ifft[i,:],label="string"+str(i))
-    plt.title("Road")
-    plt.xlabel("Number of pixel")
+    variance_of_image = np.var(image_array)
+    print(variance_of_image,np.sum(image_array))
+    ifft_matr= ACF_by_periodogram(image_array)
+
+    # lst10= image_array[0,:]
+    # lst500 = image_array[300, :]
+    # lst1000 = image_array[600, :]
+    # lst900 = image_array[900, :]
+    # print(len(lst10))
+    # ifft10= ACF_by_periodogram(list_10)
+    # ifft500 = ACF_by_periodogram(list_500)
+    # ifft1000 = ACF_by_periodogram(list_1000)
+    # ifft900 = ACF_by_periodogram(lst900)
+
+    # ifft500= ACF_by_periodogram(list_500)
+    # ifft1000 = ACF_by_periodogram(list_1000)
+    # # for i in range(0,1001,300):
+    #     plt.plot(ifft[i,:],label="string"+str(i))
+
+    print(ifft_matr[0,0])
+    cor_matr = ifft_matr/ifft_matr[0,0]
+
+    for i in range(0,1000,300):
+        plt.plot(ifft_matr[i,:],label="string " + str(i))
+    # plt.plot(ifft500, label="pixel[500][500]")
+    # plt.plot(ifft1000, label="pixel[1000][1000]")
+    # plt.plot(ifft900, label="string 900")
+    plt.title("Road. 2200 frame")
+    plt.xlabel("Number of frame")
     plt.ylabel("ACF")
     plt.legend()
     plt.show()
-    acorr = sm.tsa.acf(list_00, nlags=100)
 
-    list_ACF = []
-    for i in range(1000):
-        list_ACF.append(calc_ACF(10, 10, i))
-    print(list_ACF)
 
-    plt.plot(list_ACF[0:10])
-    plt.plot(acorr)
-    plt.show()
-    pool = Pool()
+
+
+
+
+
+
+
+    # acorr = sm.tsa.acf(list_00, nlags=100)
+    #
+    # list_ACF = []
+    # for i in range(1000):
+    #     list_ACF.append(calc_ACF(10, 10, i))
+    # print(list_ACF)
+    #
+    # plt.plot(list_ACF[0:10])
+    # plt.plot(acorr)
+    # plt.show()
+    # pool = Pool()
 
     # Список параметров для вызова функции
     # parameters = [("Road.mp4", 0, 0), ("Road.mp4", 500, 500), ("Road.mp4", 1000, 1000)]
