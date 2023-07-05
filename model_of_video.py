d1d8c262cc67e753445dd5cf772f7e8509018a68
SD6399 <david-shapiro@mail.ru>
2023-07-05 10:54:12 +0300

    correcting calculate ACF and selection of parameters

--- model_of_video.py
+++ model_of_video.py
@@ -1,6 +1,8 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import product
+from sklearn.metrics import mean_squared_error
 from multiprocessing import Pool
 import statsmodels.api as sm
 import cv2
@@ -104,57 +106,92 @@ def ACF_by_periodogram(lst):
     lst_pix = np.array(lst)
 
     # fft = np.fft.fft2(image_array)
-    fft = np.fft.fft2(lst_pix)
-    print(fft[0,0])
-    fft[0]=0
+    fft = np.fft.fft(lst_pix)
+
+    # fft[0]=0
     # fft[0][0]=0
     abs_fft = np.power(np.abs(fft), 2)
-    ifft = np.fft.ifft2(abs_fft)
+    ifft = np.fft.ifft(abs_fft)
     ifft = np.abs(ifft) / fft.size
-    print(ifft.shape,len(fft))
 
     return ifft
 
 
-def calc_ACF(coord_x, coord_y, numb_frame):
-    p = 0.01
-    alf = 0.0016
-    betta = 0.01
-    r = math.sqrt(coord_x * coord_x + coord_y * coord_y)
+def calc_ACF(p, alf, betta, numb_frame):
+    # p = 0.001
+    # alf = 0.0066
+    # betta = 0.072
+    # r = math.sqrt(coord_x * coord_x + coord_y * coord_y)
 
-    R = (p * math.e ** (-alf * r) + (1 - p) * math.e ** (-betta * r)) * (
-            p * math.e ** (-alf * numb_frame) + (1 - p) * math.e ** (-betta * numb_frame))
+    # R = (p * math.e ** (-alf * r) + (1 - p) * math.e ** (-betta * r)) * \
+    R = (p * math.e ** (-alf * numb_frame) + (1 - p) * math.e ** (-betta * numb_frame))
 
     return R
 
 
-if __name__ == '__main__':
 
+def gencoordinates(m, n):
+    seen = set()
+
+    x, y = np.randint(m, n), np.randint(m, n)
+
+    while True:
+        seen.add((x, y))
+        yield (x, y)
+        x, y = np.randint(m, n), np.randint(m, n)
+        while (x, y) in seen:
+            x, y = np.randint(m, n), np.randint(m, n)
+
+
+if __name__ == '__main__':
+    rand_list = np.random.choice(1000, 1000,replace=False)
     # ACF_image()
     #
     # part_2d = bracket_1()
     # part_1d = bracket_2()
-    vidcap = cv2.VideoCapture("Road.mp4")
+    vidcap = cv2.VideoCapture("cut_RealBarca.mp4")
     success = True
-    list_10 = []
-    list_500=[]
-    list_1000=[]
-    count = 0
-    # while success:
-    #     success, image = vidcap.read()
-    #     if success:
-    #         list_10.append(image[10, 10, 0])
-    #         list_500.append(image[500, 500, 0])
-    #         list_1000.append(image[1000, 1000, 0])
-    #         print(count)
-    #     count += 1
-    image_array = io.imread("frames_orig_video/frame2200.png")[:, :, 0]
-    variance_list=np.var(image_array,axis=1)
-    image_array_rav = np.ravel(image_array)
-    variance_of_image = np.var(image_array)
-    print(variance_of_image,np.sum(image_array))
-    ifft_matr= ACF_by_periodogram(image_array)
 
+    pair_lst = [(rand_list[i-1],rand_list[i]) for i in range(1,len(rand_list))]
+    ext_list = [(rand_list[i],rand_list[i-1]) for i in range(1,len(rand_list))]
+
+    pair_lst.extend(ext_list)
+    my_square = pair_lst
+
+
+
+    matr_time = np.array([])
+    # permut = list(product([250,275,555,551, 300, 350, 375, 400, 425, 450, 475, 500, 550, 600, 650, 675, 700, 750], repeat=2))
+    # permut = list(product(my_square, repeat=2))
+    print(len(pair_lst))
+    count = 0
+
+    matr_time = np.zeros((len(pair_lst), 2048))
+
+    while success and count < 2048:
+
+        success, image = vidcap.read()
+        for i in range(len(pair_lst)):
+            # print(i)
+            matr_time[i, count] = image[pair_lst[i][0], pair_lst[i][1], 0]
+        count += 1
+    # image_array = io.imread("frames_orig_video/frame2200.png")[:, :, 0]
+    # variance_list=np.var(image_array,axis=1)
+    # image_array_rav = np.ravel(image_array)
+    # variance_of_image = np.var(image_array)
+    # print(variance_of_image,np.sum(image_array))
+    mean_matr_time = np.mean(matr_time, axis=0)
+    mean_by_mean = np.mean(mean_matr_time)
+    ifft_matr = np.zeros(matr_time.shape)
+    for i in range(matr_time.shape[0]):
+        # print(len(matr_time[i, :]))
+        ifft_matr[i, :] = ACF_by_periodogram(matr_time[i, :])
+        # ifft_matr[i, :] = ifft_matr[i, :] - np.mean(matr_time[i, :]) ** 2
+        # ifft_matr[i, :] /= np.max(ifft_matr[i, :])
+
+    mean_ifft = np.mean(ifft_matr, axis=0)
+    mean_ifft -= mean_by_mean ** 2
+    mean_ifft /= np.max(mean_ifft)
     # lst10= image_array[0,:]
     # lst500 = image_array[300, :]
     # lst1000 = image_array[600, :]
@@ -170,40 +207,65 @@ if __name__ == '__main__':
     # # for i in range(0,1001,300):
     #     plt.plot(ifft[i,:],label="string"+str(i))
 
-    print(ifft_matr[0,0])
-    cor_matr = ifft_matr/ifft_matr[0,0]
+    # print(ifft_matr[0,0])
+    # cor_matr = ifft_matr/ifft_matr[0,0]
 
-    for i in range(0,1000,300):
-        plt.plot(ifft_matr[i,:],label="string " + str(i))
-    # plt.plot(ifft500, label="pixel[500][500]")
+    # for i in range(0,1000,300):
+    #     plt.plot(ifft_matr[i,:]/np.max(ifft_matr[i,:]),label="string " + str(i))
+
+    plt.plot(mean_ifft[:200], label="Average ACF")
     # plt.plot(ifft1000, label="pixel[1000][1000]")
     # plt.plot(ifft900, label="string 900")
-    plt.title("Road. 2200 frame")
+    plt.title("Road")
     plt.xlabel("Number of frame")
     plt.ylabel("ACF")
+
+    all_mse = []
+    params = []
+    for p in np.arange(0.05, 1.01, 0.05):
+        for betta in np.arange(0.007,0.011, 0.001):
+            for alfa in np.arange(0.31,0.92,0.05):
+                list_ACF = []
+                for i in range(100):
+                    list_ACF.append(calc_ACF(p, alfa, betta, i))
+
+                params.append((p,betta,alfa))
+                all_mse.append(mean_squared_error(mean_ifft[:30], list_ACF[:30]))
+
+    print(min(all_mse),all_mse.index(min(all_mse)))
+    need_params= params[all_mse.index(min(all_mse))]
+    print(params[all_mse.index(min(all_mse))])
+
+    list_ACF = []
+    for i in range(100):
+        list_ACF.append(calc_ACF(need_params[0], need_params[1], need_params[2], i))
+
+    div_ACF_video = []
+    div_ACF_model = []
+
+    print(mean_ifft)
+
+    for i in range(1, len(list_ACF)):
+        div_ACF_video.append(mean_ifft[i] / mean_ifft[i - 1])
+        div_ACF_model.append(list_ACF[i] / list_ACF[i - 1])
+
+    from statistics import mean
+    print(mean(div_ACF_video[40:100]))
+    # print(sum(div_ACF_video[30:100]) / len(div_ACF_video[30:100]), div_ACF_video)
+    print(list(mean_ifft[:30]))
+
+    plt.title("RealBarca")
+    plt.plot(list_ACF[:200], label="Model ACF")
+    # plt.plot(acorr)
     plt.legend()
     plt.show()
-
-
-
-
-
-
-
-
-
-    # acorr = sm.tsa.acf(list_00, nlags=100)
-    #
-    # list_ACF = []
-    # for i in range(1000):
-    #     list_ACF.append(calc_ACF(10, 10, i))
-    # print(list_ACF)
-    #
-    # plt.plot(list_ACF[0:10])
-    # plt.plot(acorr)
-    # plt.show()
     # pool = Pool()
 
+    plt.title("B(k+1)/B(k). RealBarca. Volume of pixel=2000")
+    plt.plot(div_ACF_video, label="Average of video")
+    plt.plot(div_ACF_model, label="Model")
+    plt.legend()
+    plt.show()
     # Список параметров для вызова функции
     # parameters = [("Road.mp4", 0, 0), ("Road.mp4", 500, 500), ("Road.mp4", 1000, 1000)]
     #
